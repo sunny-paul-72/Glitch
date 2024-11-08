9920b5c08d7227699ac4c82c46fb1284b309d7c8
Prabhas Raj <prabhas@kali>
2024-11-08 14:54:00 +0530

    changes on file

---
+++ script.js
@@ -0,0 +1,4 @@
+// Redirect to another website after 3 seconds
+setTimeout(function() {
+    window.location.href = "https://my-cal-1.netlify.app/"; // Replace with the desired website URL
+}, 3000); // 3000ms = 3 seconds
